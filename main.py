d5d8a7d6f600bb24d439ba45df7df170b8ef9c8d
azu-cyber <avikalizhimomi@gmail.com>
2024-10-30 17:47:16 +0530

    f2

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 def f1(n):
     return n*(n+1)/2
 print(f1(4))
+#the no. of iterations is 1
 def f2(n):
     sum=0
     for i in range(1, n+1):
         sum+=i
     return sum
 print(f2(4))
+#the no. of iterations is 4
 def fun3(n):
     sum=0
     for i in range(1,n+1):
@@ -14,3 +16,4 @@ def fun3(n):
             sum+=1
     return sum
 print(fun3(4))
+#the no. of iterations is 10
